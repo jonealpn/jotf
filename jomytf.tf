13d7c28893121cac42bd26d75a320a2b3ba7a438
jonealpn <joneal@paloaltonetworks.com>
2024-10-15 11:22:26 +0000

    my commit

---
+++ jomytf.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "mytfinstance" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tfServerInstance"
+  }
+}
